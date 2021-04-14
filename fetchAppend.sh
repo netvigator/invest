d021de3ce61c6aed049c38f63689ae9950a1875b
Rick Graves <gravesricharde@yahoo.com>
2021-04-15 06:21:16 +0700

    also import & call updateFlowsFileMaybe()

--- fetchAppend.sh
+++ fetchAppend.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd ~/Devel/invest
 source ~/.virtualenvs/invest/bin/activate
-python -c "from collect import updateFileMaybe; updateFileMaybe()"
+python -c "from collect import updateAssetsFileMaybe, updateFlowsFileMaybe; updateAssetsFileMaybe(); updateFlowsFileMaybe()"
